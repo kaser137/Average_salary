6e5edc85f1b8a5a6ae0a3ff40c971ce07a174176
kaser137 <107580630+kaser137@users.noreply.github.com>
2023-01-27 02:38:43 +0200

    Divides function.

--- service_file.py
+++ service_file.py
@@ -17,43 +17,59 @@ def choice_for_average_salary(vac_attr_list, pay_from, pay_to):
     return salary
 
 
-def predict_rub_salary(api_url, target_vacancy, app_code=None, app_key=None, api_page=1):
+def predict_rub_salary_hh(api_url, target_vacancy, api_page=20):
     page = 0
     per_page = 100
-    count = 100
-    town = 4
     area = 1
     vac_found_number = 0
     vac_number = 0
     total_sum = 0
     url = api_url
     while page < api_page:
-        payload = {'code': app_code, 'app_key': app_key, 'town': town, 'count': count, 'keyword': f'{target_vacancy}',
-                   'page': f'{page}', 'per_page': per_page, 'area': area, 'vacancy_search_fields': 'name',
+        payload = {'page': f'{page}', 'per_page': per_page, 'area': area, 'vacancy_search_fields': 'name',
                    'text': f'{target_vacancy}'}
         response = requests.get(url, params=payload)
         response.raise_for_status()
         json_answer = response.json()
-        if app_code:
-            resp_json_list = json_answer['objects']
-            vac_found_number = json_answer()['total']
-            pay_from = 'payment_from'
-            pay_to = 'payment_to'
-        else:
-            resp_json_list = json_answer['items']
-            vac_found_number = json_answer['found']
-            pay_from = 'from'
-            pay_to = 'to'
+        resp_json_list = json_answer['items']
+        vac_found_number = json_answer['found']
         for vacancy in resp_json_list:
-            if not app_code:
-                if vacancy['salary']:
-                    vac_attr_list = vacancy['salary']
-                else:
-                    vac_attr_list = None
+            if vacancy['salary']:
+                vac_attr_list = vacancy['salary']
             else:
-                vac_attr_list = vacancy
+                vac_attr_list = None
             if vac_attr_list:
-                salary = choice_for_average_salary(vac_attr_list, pay_from, pay_to)
+                salary = choice_for_average_salary(vac_attr_list, 'from', 'to')
+                if salary:
+                    total_sum += salary
+                    vac_number += 1
+        page += 1
+    if vac_number:
+        return int(total_sum / vac_number), vac_number, vac_found_number
+    else:
+        return 0, 0, vac_found_number
+
+
+def predict_rub_salary_sj(api_url, target_vacancy, app_code=None, app_key=None, api_page=1):
+    page = 0
+    count = 100
+    town = 4
+    vac_found_number = 0
+    vac_number = 0
+    total_sum = 0
+    url = api_url
+    while page < api_page:
+        payload = {'code': app_code, 'app_key': app_key, 'town': town, 'count': count, 'keyword': f'{target_vacancy}',
+                   'page': f'{page}'}
+        response = requests.get(url, params=payload)
+        response.raise_for_status()
+        json_answer = response.json()
+        resp_json_list = json_answer['objects']
+        vac_found_number = json_answer['total']
+        for vacancy in resp_json_list:
+            vac_attr_list = vacancy
+            if vac_attr_list:
+                salary = choice_for_average_salary(vac_attr_list, 'payment_from', 'payment_to')
                 if salary:
                     total_sum += salary
                     vac_number += 1
@@ -67,8 +83,12 @@ def predict_rub_salary(api_url, target_vacancy, app_code=None, app_key=None, api
 def get_stats_vacancies(url, langs_list, app_code=None, app_key=None, api_page=1):
     vacancies = {}
     for lang in langs_list:
-        average_salary, vacancies_processed, vacancies_found = predict_rub_salary(url, f'программист {lang}', app_code,
-                                                                                  app_key, api_page)
+        if app_code:
+            average_salary, vacancies_processed, vacancies_found = predict_rub_salary_sj(url, f'программист {lang}',
+                                                                                         app_code, app_key, api_page)
+        else:
+            average_salary, vacancies_processed, vacancies_found = predict_rub_salary_hh(url, f'программист {lang}',
+                                                                                         api_page)
         vacancies[lang] = {'vacancies_found': vacancies_found, 'vacancies_processed': vacancies_processed,
                            'average_salary': average_salary}
     return vacancies
