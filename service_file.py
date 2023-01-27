4cf3fae88885fe9e58ff009aec4bdf81c5ac9f98
kaser137 <107580630+kaser137@users.noreply.github.com>
2023-01-27 02:55:11 +0200

    Change names of variables.

--- service_file.py
+++ service_file.py
@@ -2,14 +2,14 @@ import requests
 from terminaltables import AsciiTable
 
 
-def choice_for_average_salary(vac_attr_list, pay_from, pay_to):
-    if vac_attr_list['currency'] in ("RUR", 'rub', None):
-        if vac_attr_list[pay_from] and vac_attr_list[pay_to]:
-            salary = (vac_attr_list[pay_from] + vac_attr_list[pay_to]) / 2
-        elif vac_attr_list[pay_from]:
-            salary = vac_attr_list[pay_from] * 1.2
-        elif vac_attr_list[pay_to]:
-            salary = vac_attr_list[pay_to] * 0.8
+def choice_for_average_salary(vacancy_attr_list, pay_from, pay_to):
+    if vacancy_attr_list['currency'] in ("RUR", 'rub', None):
+        if vacancy_attr_list[pay_from] and vacancy_attr_list[pay_to]:
+            salary = (vacancy_attr_list[pay_from] + vacancy_attr_list[pay_to]) / 2
+        elif vacancy_attr_list[pay_from]:
+            salary = vacancy_attr_list[pay_from] * 1.2
+        elif vacancy_attr_list[pay_to]:
+            salary = vacancy_attr_list[pay_to] * 0.8
         else:
             salary = None
     else:
@@ -21,8 +21,8 @@ def predict_rub_salary_hh(api_url, target_vacancy, api_page=20):
     page = 0
     per_page = 100
     area = 1
-    vac_found_number = 0
-    vac_number = 0
+    vacancies_found_quantity = 0
+    vacancies_quantity = 0
     total_sum = 0
     url = api_url
     while page < api_page:
@@ -32,7 +32,7 @@ def predict_rub_salary_hh(api_url, target_vacancy, api_page=20):
         response.raise_for_status()
         json_answer = response.json()
         resp_json_list = json_answer['items']
-        vac_found_number = json_answer['found']
+        vacancies_found_quantity = json_answer['found']
         for vacancy in resp_json_list:
             if vacancy['salary']:
                 vac_attr_list = vacancy['salary']
@@ -42,20 +42,20 @@ def predict_rub_salary_hh(api_url, target_vacancy, api_page=20):
                 salary = choice_for_average_salary(vac_attr_list, 'from', 'to')
                 if salary:
                     total_sum += salary
-                    vac_number += 1
+                    vacancies_quantity += 1
         page += 1
-    if vac_number:
-        return int(total_sum / vac_number), vac_number, vac_found_number
+    if vacancies_quantity:
+        return int(total_sum / vacancies_quantity), vacancies_quantity, vacancies_found_quantity
     else:
-        return 0, 0, vac_found_number
+        return 0, 0, vacancies_found_quantity
 
 
 def predict_rub_salary_sj(api_url, target_vacancy, app_code=None, app_key=None, api_page=1):
     page = 0
     count = 100
     town = 4
-    vac_found_number = 0
-    vac_number = 0
+    vacancies_found_quantity = 0
+    vacancies_quantity = 0
     total_sum = 0
     url = api_url
     while page < api_page:
@@ -65,19 +65,19 @@ def predict_rub_salary_sj(api_url, target_vacancy, app_code=None, app_key=None, 
         response.raise_for_status()
         json_answer = response.json()
         resp_json_list = json_answer['objects']
-        vac_found_number = json_answer['total']
+        vacancies_found_quantity = json_answer['total']
         for vacancy in resp_json_list:
             vac_attr_list = vacancy
             if vac_attr_list:
                 salary = choice_for_average_salary(vac_attr_list, 'payment_from', 'payment_to')
                 if salary:
                     total_sum += salary
-                    vac_number += 1
+                    vacancies_quantity += 1
         page += 1
-    if vac_number:
-        return int(total_sum / vac_number), vac_number, vac_found_number
+    if vacancies_quantity:
+        return int(total_sum / vacancies_quantity), vacancies_quantity, vacancies_found_quantity
     else:
-        return 0, 0, vac_found_number
+        return 0, 0, vacancies_found_quantity
 
 
 def get_stats_vacancies(url, langs_list, app_code=None, app_key=None, api_page=1):
@@ -95,12 +95,12 @@ def get_stats_vacancies(url, langs_list, app_code=None, app_key=None, api_page=1
 
 
 def draw_table(user_dictionary, title):
-    table_data = []
+    table_data_list = []
     heading = ['Язык программирования', 'Вакансий найдено ', 'Вакансий обработано', 'Средняя зарплата ']
-    table_data.append(heading)
+    table_data_list.append(heading)
     for key in user_dictionary:
-        table_data.append([item for item in user_dictionary[key].values()])
-        table_data[-1].insert(0, key)
+        table_data_list.append([item for item in user_dictionary[key].values()])
+        table_data_list[-1].insert(0, key)
     title = title
-    table = AsciiTable(table_data, title)
+    table = AsciiTable(table_data_list, title)
     print('\n', table.table)
