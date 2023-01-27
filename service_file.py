6e951e60801213d9b47d0fd72b499e31c57da992
kaser137 <107580630+kaser137@users.noreply.github.com>
2023-01-28 01:58:20 +0200

    Change variables names.

--- service_file.py
+++ service_file.py
@@ -74,9 +74,9 @@ def predict_rub_salary_sj(api_url, target_vacancy, app_code=None, app_key=None, 
         return 0, 0, vacancies_found_quantity
 
 
-def get_stats_vacancies_hh(url, langs_list, api_page=20):
+def get_stats_vacancies_hh(url, languages, api_page=20):
     vacancies = {}
-    for lang in langs_list:
+    for lang in languages:
         average_salary, vacancies_processed, vacancies_found = predict_rub_salary_hh(url, f'программист {lang}',
                                                                                      api_page)
         vacancies[lang] = {'vacancies_found': vacancies_found, 'vacancies_processed': vacancies_processed,
